28800bfc737c55dc66d8cf6e07bed67aa54be6ce
masdefry <muhammaddefryan@gmail.com>
2025-01-23 10:54:57 +0700

    feat: login with username and password

--- intro.js
+++ intro.js
@@ -1,1 +1,11 @@
 console.log('Intro to Git')
+
+/*
+    git add .
+
+    git commit -m "message"
+
+    git remote add origin linkGithubRepo   ---> One time
+
+    git push origin master
+*/
---
+++ login.js
@@ -0,0 +1,2 @@
+console.log('Input Your Username')
+console.log('Input Your Password')
